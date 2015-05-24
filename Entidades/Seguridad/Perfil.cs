1afd36ec56e9a8db206a3f3ed8a6276ade2768d4
ethedy <ethedy@gmail.ccom>
2015-05-23 21:07:33 -0300

    Cambio nombres para coincidir con diagramas Cambio DB por Database Agrego documentacion en clases y miembros

--- Entidades/Seguridad/Perfil.cs
+++ Entidades/Seguridad/Perfil.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades.Seguridad
 {
-  public class Permiso
+  public class Perfil
   {
   }
 }
---
+++ Terminal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Servicios;
+
+namespace Terminal
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string usr, pass;
+
+      Console.WriteLine("Ingrese usuario y password");
+      
+      Console.Write("User -->  ");
+      usr = Console.ReadLine();
+
+      Console.Write("Password -->  ");
+      Console.ForegroundColor = ConsoleColor.Black;
+      pass = Console.ReadLine();
+      Console.ResetColor();
+
+      SecurityServices security = new SecurityServices();
+
+      Sesion ses = security.Login(usr, pass);
+
+      if (ses == null)
+        Console.WriteLine("ERROR!! Verificar credenciales");
+      else
+      {
+        Console.WriteLine("Bienvenido {0}. Su password expira el {1}", ses.FullName, ses.FechaExpiracion );
+      }
+
+      Console.WriteLine("Press any key");
+      Console.ReadLine();
+    }
+  }
+}
